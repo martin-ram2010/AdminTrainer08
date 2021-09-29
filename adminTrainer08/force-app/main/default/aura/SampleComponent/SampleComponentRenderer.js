fb8c1f42d4478a77b5b8ff2c3ef8742dbda87959
martin ram <martin.ram2010@gmail.com>
2021-09-29 19:27:29 +0530

    Lightning Component Introduction

--- adminTrainer08/force-app/main/default/aura/SampleComponent/SampleComponentRenderer.js
+++ adminTrainer08/force-app/main/default/aura/SampleComponent/SampleComponentRenderer.js
@@ -1,3 +1,24 @@
 ({
 	// Your renderer method overrides go here
+	render : function(component, helper) {
+      var returnVal = this.superRender();
+	  // Write your custom code here.
+	  console.log('render');
+	  return returnVal; 
+   },
+   rerender : function(component, helper) {
+	  this.superRerender();
+	  // Write your custom code here. 
+	  console.log('rerender');
+   },
+   afterRender : function(component, helper) {
+	 this.superAfterRender();
+	  // Write your custom code here. 
+	  console.log('afterRender');
+   },
+   unRender : function(component, helper) {
+	 this.superUnrender();
+	  // Write your custom code here. 
+	  console.log('unRender');
+   }
 })
